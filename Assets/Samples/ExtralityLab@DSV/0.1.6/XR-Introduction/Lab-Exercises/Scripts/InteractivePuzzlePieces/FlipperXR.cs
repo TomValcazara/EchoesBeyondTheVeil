1dd62de692d75ff11562f4354c5154821f34ad79
Fernando Valcazara <66259024+FernandoValcazara@users.noreply.github.com>
2026-02-10 20:32:31 +0100

    Initial Empty VR Project

---
+++ Assets/Samples/ExtralityLab@DSV/0.1.6/XR-Introduction/Lab-Exercises/Scripts/InteractivePuzzlePieces/FlipperXR.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperXR : InteractivePuzzlePieceXR<HingeJoint>
+{
+    [Range(0f, 4096f)]
+    public float power = 800f;
+
+    void Awake ()
+    {
+        JointMotor flipperMotor = physicsComponent.motor;
+        flipperMotor.targetVelocity = power;
+        physicsComponent.motor = flipperMotor;
+    }
+    
+    public void SetPower(float newPower)
+    {
+        //power = newPower;
+        power = Mathf.Clamp(newPower, 0f, 4096f);
+        JointMotor motor = physicsComponent.motor;
+        motor.targetVelocity = power;
+        physicsComponent.motor = motor;
+    }
+
+    protected override void ApplyActiveState ()
+    {
+        physicsComponent.useMotor = true;
+    }
+
+    protected override void ApplyInactiveState ()
+    {
+        physicsComponent.useMotor = false;
+    }
+}
---
+++ Assets/Samples/ExtralityLab@DSV/0.1.6/XR-Introduction/Puzzle Creator/Scripts/SceneSystems/SceneCompletion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneCompletion : MonoBehaviour
+{
+    public ScreenFader screenFader;
+    public GameObject panel;
+    public WebSocketClientExample WebSocketClient;
+
+    // Called when target of level is achieved
+    public void CompleteLevel (float time)
+    {
+        panel.SetActive (true);
+    }
+
+    // UI Button
+    public void ReloadLevel ()
+    {
+        screenFader.FadeOut(() => SceneManager.LoadSceneAsync (gameObject.scene.buildIndex));
+        WebSocketClient.SendLEDWinOFF();
+    }
+}
